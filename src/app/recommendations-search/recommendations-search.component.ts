fac3dec81751f80406e9e0b7bd5ee90dc7515746
Jeremias Lemm <jeremias.lemm@rwth-aachen.de>
2019-04-05 15:09:00 +0200

    redirect to recommendations list after search

--- src/app/recommendations-search/recommendations-search.component.ts
+++ src/app/recommendations-search/recommendations-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, NgForm, Validators, FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 export interface Source {
   value: number;
@@ -15,6 +16,7 @@ export interface Source {
 export class RecommendationsSearchComponent implements OnInit {
 
   recommendationsForm: FormGroup;
+  router: Router;
 
   sources: Source[] = [
     {value: 1, name: 'Auto'},
@@ -23,17 +25,19 @@ export class RecommendationsSearchComponent implements OnInit {
     {value: 4, name: 'Proxer'}
   ];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, _router: Router) {
     this.recommendationsForm = fb.group({
       'Username' : [null, Validators.required],
       'Source' : [1, Validators.required]
     });
+    this.router = _router;
   }
 
   ngOnInit() {
   }
 
   onFormSubmit(form: NgForm) {
+    this.router.navigateByUrl('/recommendations');
     console.log(form);
   }
 
